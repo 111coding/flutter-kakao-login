2f18a7445e6da001eeed9998e63dabff246388fb
111coding <jwlee1024@gmail.com>
2026-01-19 10:00:34 +0900

    feat: 파이어베이스 로그인 연동

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 f4b26b6a77e084ecbb5933b9fe8d1c5f
+
+https://www.notion.so/teamsparta/Firebase-Authentication-Kakao-JWT-2e62dc3ef51480f6b57dd6f3492e7723
+
+```sh
+# 아래의 메시지가 콘솔에 뜬다면
+# The operation couldn’t be completed. Unable to locate a Java Runtime.
+# Please visit http://www.java.com for information on installing Java.
+# 이 명령어 사용
+# Mac
+/Applications/Android\ Studio.app/Contents/jbr/Contents/Home/bin/keytool -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore -storepass android -keypass android | openssl sha1 -binary | openssl base64
+# Windows CMD
+"C:\Program Files\Android\Android Studio\jbr\bin\keytool.exe" -exportcert -alias androiddebugkey -keystore "%USERPROFILE%\.android\debug.keystore" -storepass android -keypass android | openssl sha1 -binary | openssl base64
+# Windows CMD 에서 openssl 찾을 수 없다고 에러 뜨면
+# Git Bash 에서 아래 명령어 입력
+"/c/Program Files/Android/Android Studio/jbr/bin/keytool" -exportcert -alias androiddebugkey -keystore ~/.android/debug.keystore -storepass android -keypass android | openssl sha1 -binary | openssl base64
+
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao_login/firebase_options.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   KakaoSdk.init(nativeAppKey: "f4b26b6a77e084ecbb5933b9fe8d1c5f");
   runApp(const MyApp());
 }
@@ -67,6 +72,18 @@ class _MyHomePageState extends State<MyHomePage> {
         : await UserApi.instance.loginWithKakaoAccount();
 
     print(token.idToken);
+    // 1. 카카오에서 발급받은 상자 파이어베이스에서 사용되는 객체로 감싸기
+    OAuthProvider oAuthP = OAuthProvider("oidc.oidc.kakao");
+    OAuthCredential credential = oAuthP.credential(
+      accessToken: token.accessToken,
+      idToken: token.idToken,
+    );
+
+    // 2. 로그인 시키기
+    UserCredential userCred = await FirebaseAuth.instance.signInWithCredential(
+      credential,
+    );
+    print(await userCred.user?.getIdToken());
 
     setState(() {
       // This call to setState tells the Flutter framework that something has
